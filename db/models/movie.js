8ebeef5d1fc1a837875bf83a47f2b147d2b7da39
Alan McClenaghan <AlanM@Alans-MBP-2.cust.communityfibre.co.uk>
2024-02-17 20:50:54 +0000

    Go Further with Models

--- db/models/movie.js
+++ db/models/movie.js
@@ -1,10 +1,30 @@
 const Sequelize = require('sequelize');
 
 module.exports = (sequelize) => {
-  class Movie extends Sequelize.Model {}
-  Movie.init({
-    title: Sequelize.STRING,
-  }, { sequelize });
-
-  return Movie;
-};
+    class Movie extends Sequelize.Model {}
+    Movie.init({
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false, // disallow null
+      },
+      runtime: {
+        type: Sequelize.INTEGER,
+        allowNull: false, // disallow null
+      },
+      releaseDate: {
+        type: Sequelize.DATEONLY,
+        allowNull: false, // disallow null
+      },
+      isAvailableOnVHS: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false, // disallow null
+      },
+    }, { sequelize });
+  
+    return Movie;
+  };
